81f6d99e960d51a7590f6d20453848e79f3be9b9
sofiase <sofiase@student.chalmers.se>
2013-04-25 14:41:49 +0200

    started with changes for Eat-button and needed to pull before able to continue

--- src/edu/chl/dat255/sofiase/readyforapet/PetActivity.java
+++ src/edu/chl/dat255/sofiase/readyforapet/PetActivity.java
@@ -3,40 +3,26 @@ package edu.chl.dat255.sofiase.readyforapet;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class PetActivity extends Activity {
+public class PetActivity extends Activity{
+	
+	
 
+	
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate (savedInstanceState);
-		setContentView(R.layout.petactivity);
-		
-		// Get the pet name from the intent
-		Intent nameintent = getIntent();
-		String petname = nameintent.getStringExtra(CreatePet.EXTRA_MESSAGE);
-		
-		//Create the text view
-		TextView petgreeting = new TextView(this);
-		petgreeting.setTextSize(40);
-		
-		if(petname != null){
-			
-			petgreeting.setText("Hello, my name is " + petname + "!");
 	
-		}
 		
-		else{
-		
-			petgreeting.setText(" Hi budy, I've missed you!");
-			
-		}
-		
-		//Set the pet greeting as the activity layout
-		setContentView(petgreeting);
+
 	}
+	
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -48,5 +34,7 @@ public class PetActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 	//"Add the title string" - 75% av scrollern
+
+
 
 }
